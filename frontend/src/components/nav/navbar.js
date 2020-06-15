c5f04331e3166748a2d928a5d3cc8da06bda75e7
CG <cgodlew@gmail.com>
2020-06-15 11:13:37 -0400

    Updated references in Nav Container

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -19,9 +19,9 @@ class NavBar extends React.Component {
       if (this.props.loggedIn) {
         return (
             <div>
-                <Link to={'/tweets'}>All Tweets</Link>
+                <Link to={'/FIXTHIS'}>####TBD</Link>
                 <Link to={'/profile'}>Profile</Link>
-                <Link to={'/new_tweet'}>Write a Tweet</Link>
+                <Link to={'/FIXTHIS'}>####TBD</Link>
                 <button onClick={this.logoutUser}>Logout</button>
             </div>
         );
@@ -38,7 +38,7 @@ class NavBar extends React.Component {
   render() {
       return (
         <div>
-            <h1>Chirper</h1>
+            <h1>####UPDATE</h1>
             { this.getLinks() }
         </div>
       );
