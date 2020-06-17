e43146fb98d7e2510742b1d7ae556d79e69265c2
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-16 23:04:00 -0400

    It was Carl's fault

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require("path");
 
 
 const users = require("./routes/api/users");
 const reviews = require("./routes/api/reviews");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
