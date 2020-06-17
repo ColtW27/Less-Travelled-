7450700bba5330d07e7713b0eab77d77fa048cf7
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-16 22:39:22 -0400

    Heroku env variables

---
+++ config/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:fre-4de-42sas@cluster0-ayxd6.mongodb.net/dev?retryWrites=true&w=majority",
+  //Make sure this is your own unique string
+  secretOrKey: "secret",
+};
---
+++ config/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI_KEY,
+  //Make sure this is your own unique string
+  secretOrKey: process.env.SECRET_OR_KEY,
+};
