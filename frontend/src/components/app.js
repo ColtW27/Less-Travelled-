fe2ad39e46ff48dae8b8df827bbc385c0a6559b6
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-18 09:34:45 -0400

    Protect route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 // import BackgroundVideo from './backgroundVideo/backgroundVideo';
-
+import AttractionsForm from './attraction_form/attraction_form_container'
 
 import AttractionsContainer from './search/attractions_container'
 
@@ -31,6 +31,7 @@ const App = () => (
         {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> ####*/}
 
 
+        <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
         <AuthRoute exact path="/attractions" component={AttractionsContainer} />
 
     </Switch>
