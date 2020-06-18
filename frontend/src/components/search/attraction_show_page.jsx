f7e5e6f3a21b680030689c300db04bdd0991691b
Rahul Jain <rahulj93@gmail.com>
2020-06-17 20:06:38 -0400

    attractions index and show page progress

--- frontend/src/components/search/attraction_show_page.jsx
+++ frontend/src/components/search/attraction_show_page.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import BenchDetail from './bench_detail';
-import BenchMap from '../bench_map/bench_map';
-import ReviewFormContainer from './review_form_container';
+import AttractionDetail from './attraction_detail';
+// import BenchMap from '../bench_map/bench_map';
+// import ReviewFormContainer from './review_form_container';
 import { ProtectedRoute } from '../../util/route_util';
-import { ReviewLink } from '../../util/link_util';
+// import { ReviewLink } from '../../util/link_util';
 
-const BenchShow = ({ bench, benchId, fetchBench, reviews }) => {
-  const benches = {
-    [benchId]: bench
+const AttractionShow = ({ attraction, attractionId, fetchAttraction, reviews }) => {
+  const attractions = {
+    [attractionId]: attraction 
   };
 
   return (
-    <div className="single-bench-show">
-      <div className="single-bench-map">
-        <Link to="/">Back to Benches Index</Link>
-        <BenchMap
-          benches={benches}
-          benchId={benchId}
-          singleBench={true}
-          fetchBench={fetchBench}
+    <div className="single-attraction-show">
+      <div className="single-attraction-map">
+        <Link to="/">Back to Attractions Index</Link>
+        <AttractionMap
+          attractions={attractions}
+          attractionId={attractionId}
+          singleAttraction={true}
+          fetchAttraction={fetchAttraction}
         />
       </div>
-      <div className="right-half bench-details">
-        <BenchDetail bench={bench} reviews={reviews} />
+      <div className="right-half attraction-details">
+        <AttractionDetail attraction={attraction} reviews={reviews} />
         <ReviewLink
           component={ReviewFormContainer}
-          to={`/benches/${benchId}/review`}
+          to={`/attractions/${attractionId}/review`}
           label="Leave a Review"
         />
         <ProtectedRoute
-          path="/benches/:benchId/review"
+          path="/attractions/:attractionId/review"
           component={ReviewFormContainer}
         />
       </div>
