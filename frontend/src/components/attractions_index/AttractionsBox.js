49bad9d644d01a217bc9255a9443c57c576e11fc
Rahul Jain <rahulj93@gmail.com>
2020-06-18 21:01:16 -0400

    attractions index and show pages

--- frontend/src/components/attractions_index/AttractionsBox.js
+++ frontend/src/components/attractions_index/AttractionsBox.js
@@ -15,21 +15,22 @@ import {Link} from 'react-router-dom';
 
 class AttractionBox extends React.Component {
 
-  handleClick() {
+  // handleClick() {
 
-  }
+  // }
 
   render () {
     return (
       <Link to={`/attractions/${this.props.attraction._id}`}>
         <div> 
           <div className="attractionContent">
-            <h1>{this.props.attraction._id}</h1>
+            {/* <h1>{this.props.attraction._id}</h1> */}
+            <h2>{this.props.attraction.name}</h2>
+            <br/> 
             <h2>Address: {this.props.attraction.address}</h2>
-            <h2>Name: {this.props.attraction.name}</h2>
-            <h2>Location_data: {this.props.attraction.location_data}</h2>
-            <h1>Description: {this.props.attraction.description || "No description available"}</h1>
+            {/* <h2>Location_data: {this.props.attraction.location_data}</h2> */}
             <h2>Rating: {this.props.attraction.rating}</h2>
+            <h5>Description: {this.props.attraction.description || "No description available"}</h5>
           </div>
 
           <div className="attractionPic">
