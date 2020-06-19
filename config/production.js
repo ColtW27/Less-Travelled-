f85ff41f9ec4a82fb66d5da24aaec961d5061560
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-19 13:17:59 -0400

    Google key

--- config/production.js
+++ config/production.js
@@ -2,4 +2,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI_KEY,
   //Make sure this is your own unique string
   secretOrKey: process.env.SECRET_OR_KEY,
+  googleKey: process.env.REACT_APP_GOOGLEMAP_API_KEY
+
 };
