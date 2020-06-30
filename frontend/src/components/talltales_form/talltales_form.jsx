0c11d189d44627accae7221930dba66f7ae1c7bc
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-29 22:03:25 -0400

    Basic from container

---
+++ frontend/src/components/talltales_form/talltales_form.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Talltalesform = () => {
+
+}
+
+export default Talltalesform;
---
+++ frontend/src/components/talltales_form/talltales_form_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { createTalltale } from '../../util/talltale_api_util';
+import Talltalesform from './talltales_form';
+
+
+const mSTP = state => ({
+
+});
+
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(Talltalesform);
