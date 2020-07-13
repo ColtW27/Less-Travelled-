cf3573ad44315fd4b5334aba63718f3e568e316d
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-07-13 13:40:06 -0400

    Clean up unused code

--- frontend/src/components/attractions_index/attractionsIndex.js
+++ frontend/src/components/attractions_index/attractionsIndex.js
@@ -7,7 +7,7 @@ import SearchBar from '../search/search'
 class AttractionsIndex extends React.Component {
   constructor(props) {
     super(props);
-    // this.renderAttractions = this.renderAttractions.bind(this);
+
     this.state = {
       attractions: this.props.attractions,
       searchTerm: ""
@@ -23,14 +23,12 @@ class AttractionsIndex extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if(prevState.searchTerm !== this.state.searchTerm){
-      // this.props.fetchAttractions();
       this.setState({attractions: this.props.attractions.filter(attraction => attraction.name
         .toLowerCase()
         .includes(this.state.searchTerm
           .toLowerCase()))})
     }
     if(prevProps.attractions !== this.props.attractions){
-      // this.props.fetchAttractions();
       this.setState({attractions: this.props.attractions.filter(attraction => attraction.name
         .toLowerCase()
         .includes(this.state.searchTerm
@@ -38,9 +36,6 @@ class AttractionsIndex extends React.Component {
     }
   }
   
-  // componentWillReceiveProps(newState) {
-  //   this.setState({ attractions: newState.attractions });
-  // }
 
   handleQuery(query){
     return (e) => {
@@ -87,49 +82,3 @@ export default withRouter (AttractionsIndex);
 
 
 
-//  from attraction_index_item file that I deleted: 
-
-
-// import React from 'react';
-// import { withRouter } from 'react-router-dom';
-
-// // import AttractionShowContainer from '../attraction_show_container';
-
-// class IndexItem extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     const attractionId = this.props.attraction.id;
-//     this.props.history.push(`/attractions/${attractionId}`);
-//   }
-
-//   render() {
-//     // const { name, address, location_data, description, rating, picture_url } = this.props.attraction;
-//     const { name, address, location_data, description, rating } = this.props.attraction;
-//     return (
-//       <div className="attraction-index-item"
-//         onClick={this.handleClick} >
-
-//           <div className="attraction-item-info">
-//             <span>{address}</span>
-//           </div>
-// {/* 
-//         <div className="attraction-item-info">
-//           <span className="index-item-category">Rating:</span>
-//             <span className="index-item-copy">
-//               {average_rating || 'No reviews yet'}
-//             </span>
-//           <span className="index-item-category">Description:</span>
-//           <span className="index-item-copy">{description}</span>
-//         </div> */}
-
-//         {/* <img src={picture_url} /> */}
-//       </div>
-//     );
-//   }
-// }
-
-// export default withRouter(IndexItem);
