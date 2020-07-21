48b161d62753a8c9fc765c17ff1f5cf601ed5a77
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-07-21 19:59:56 -0400

    Clean up routing code

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,42 +3,30 @@ import { AuthRoute, ProtectedRoute} from '../util/route_util';
 import {Route} from 'react-router-dom'
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-// import BackgroundVideo from './backgroundVideo/backgroundVideo';
 import TalltalesForm from './talltales_form/talltales_form_container'
 import AttractionsForm from './attraction_form/attraction_form_container'
-
 import AttractionsContainer from './attractions_index/attractions_container'
-
 import AttractionsShowContainer from './search/attraction_show_container';
 import PleaseLogin from './please_login/please_login';
 import TalltalesShowContainer from './talltales_index/talltales_container'
-// import AttractionShowContainer from './search/attraction_show_container';
-// import LoginFormContainer from './modal/session_form/login_form_container';
 import Modal from './modal/modal'
 import MainPage from './main/main_page';
-
-
 
 const App = () => (
   <div>
     <NavBarContainer />
-    <Modal />
+    <Modal/>
 
     <Switch>
-
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/attractions" component={AttractionsContainer} />
-        <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
-        <AuthRoute exact path="/attractions" component={AttractionsContainer} />
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/attractions" component={AttractionsContainer} />
+      <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
+      <AuthRoute exact path="/attractions" component={AttractionsContainer} />
       <Route path="/attractions/:id" component={AttractionsShowContainer}/>
-      <Route
-         exact path="/login" component={PleaseLogin}/>
-      <Route
-         exact path="/talltales" component={TalltalesShowContainer}/>
-
-        <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
-        <ProtectedRoute exact path="/talltalesform" component={TalltalesForm} />
-
+      <Route exact path="/login" component={PleaseLogin}/>
+      <Route exact path="/talltales" component={TalltalesShowContainer}/>
+      <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
+      <ProtectedRoute exact path="/talltalesform" component={TalltalesForm} />
     </Switch>
   </div>
 );
