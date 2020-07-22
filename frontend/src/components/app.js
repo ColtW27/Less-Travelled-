23df1db9f7d436d2177ce5049b6071a765423de8
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-07-22 10:24:39 -0400

    Clean app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { AuthRoute, ProtectedRoute} from '../util/route_util';
-import { Route, Switch} from 'react-router-dom';
+import { ProtectedRoute } from '../util/route_util';
+import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
-import Modal from './modal/modal'
+import Modal from './modal/modal';
 import MainPage from './main/main_page';
-import AttractionsContainer from './attractions_index/attractions_container'
-import AttractionsForm from './attraction_form/attraction_form_container'
-import TalltalesForm from './talltales_form/talltales_form_container'
+import AttractionsContainer from './attractions_index/attractions_container';
 import AttractionsShowContainer from './search/attraction_show_container';
 import PleaseLogin from './please_login/please_login';
-import TalltalesShowContainer from './talltales_index/talltales_container'
+import TalltalesShowContainer from './talltales_index/talltales_container';
+import AttractionsForm from './attraction_form/attraction_form_container';
+import TalltalesForm from './talltales_form/talltales_form_container';
 
 const App = () => (
   <div>
@@ -18,7 +18,6 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={MainPage} />
       <Route exact path="/attractions" component={AttractionsContainer} />
-      <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
       <Route path="/attractions/:id" component={AttractionsShowContainer}/>
       <Route exact path="/login" component={PleaseLogin}/>
       <Route exact path="/talltales" component={TalltalesShowContainer}/>
