d5786c35f6fb12f6f96103cc78115f0e8b5cbc76
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-08-26 15:47:44 -0400

    Prettified about us to aid in readability

--- frontend/src/components/about_us/about_us.jsx
+++ frontend/src/components/about_us/about_us.jsx
@@ -34,33 +34,32 @@ class AboutUs extends React.Component {
             </ul>
           </div>
         </div>
-              <div className="about-us-team-item carl grow">
+        
+        <div className="about-us-team-item carl grow">
           <div>
-              <img src="../../../Carl_profile.JPG" alt="Carl Profile"/>
-                <h2>Carl Godlewski</h2>
-                <h3>Front End Lead</h3>
-
-                <ul>
+            <img src="../../../Carl_profile.JPG" alt="Carl Profile"/>
+              <h2>Carl Godlewski</h2>
+              <h3>Front End Lead</h3>
+            <ul>
               <li><a href="https://www.linkedin.com/in/carl-godlewski-b64b0512/"target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
               <li><a href="https://angel.co/u/carl-godlewski" target="_blank" rel="noopener noreferrer">AngelList</a></li>
               <li><a href="https://cgodl.github.io/" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
               <li><a href="https://github.com/CGodl/" target="_blank" rel="noopener noreferrer">Github</a></li>
-
-                </ul>
+            </ul>
           </div>
         </div>
 
-              <div className="about-us-team-item jon grow">
+        <div className="about-us-team-item jon grow">
           <div>
-              <img src="../../../Jon_profile.jpeg" alt="Jon Profile"/>
-                <h2>Jon Romero</h2>
-                <h3>Back End Lead</h3>
-              <ul>
-                <li><a href="https://www.linkedin.com/in/jonathan-romero-ab325a165/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                <li><a href="https://angel.co/u/jonathan-romero-3" target="_blank" rel="noopener noreferrer">AngelList</a></li>
-                <li><a href="https://Duskstone.github.io" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
-                <li><a href="https://github.com/duskstone" target="_blank" rel="noopener noreferrer">Github</a></li>
-              </ul>
+            <img src="../../../Jon_profile.jpeg" alt="Jon Profile"/>
+              <h2>Jon Romero</h2>
+              <h3>Back End Lead</h3>
+            <ul>
+              <li><a href="https://www.linkedin.com/in/jonathan-romero-ab325a165/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://angel.co/u/jonathan-romero-3" target="_blank" rel="noopener noreferrer">AngelList</a></li>
+              <li><a href="https://Duskstone.github.io" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
+              <li><a href="https://github.com/duskstone" target="_blank" rel="noopener noreferrer">Github</a></li>
+            </ul>
           </div>
         </div>
       </div>
