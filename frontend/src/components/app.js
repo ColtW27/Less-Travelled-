e36af9300959a64bb2f8248891bdd39b3685fe9e
CG <cgodlew@gmail.com>
2020-11-15 01:02:27 -0500

    Redesigned about us section, have temporary splash page outline, updated scss styling for about us

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,13 +3,15 @@ import { ProtectedRoute } from '../util/route_util';
 import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import Modal from './modal/modal';
-import MainPage from './main/main_page';
+import MainPage from './splash_page/main_page';
+import SplashPage from './splash_page/second_page';
 import AttractionsContainer from './attractions_index/attractions_container';
 import AttractionsShowContainer from './search/attraction_show_container';
 import PleaseLogin from './please_login/please_login';
 import TalltalesShowContainer from './talltales_index/talltales_container';
 import AttractionsForm from './attraction_form/attraction_form_container';
 import TalltalesForm from './talltales_form/talltales_form_container';
+import AboutUsNew from "./about_us/about_usvr2";
 import AboutUs from "./about_us/about_us";
 const App = () => (
   <div>
@@ -17,11 +19,13 @@ const App = () => (
       <Modal/>
     <Switch>
       <Route exact path="/" component={MainPage} />
+      <Route exact path="/splashpage" component={SplashPage} />
       <Route exact path="/attractions" component={AttractionsContainer} />
       <Route path="/attractions/:id" component={AttractionsShowContainer}/>
       <Route exact path="/login" component={PleaseLogin}/>
       <Route exact path="/talltales" component={TalltalesShowContainer}/>
       <Route exact path="/aboutus" component={AboutUs} />
+      <Route exact path="/aboutusnew" component={AboutUsNew} />
       <ProtectedRoute exact path="/attractionsform" component={AttractionsForm} />
       <ProtectedRoute exact path="/talltalesform" component={TalltalesForm} />
     </Switch>
