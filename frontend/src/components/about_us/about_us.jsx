ccb42da62a6a914391c34d7d2f661f191d507d7f
CG <cgodlew@gmail.com>
2020-11-15 13:52:58 -0500

    Updated aboutus page to new version Have secondary splash page as WIP

--- frontend/src/components/about_us/about_us.jsx
+++ frontend/src/components/about_us/about_us.jsx
@@ -1,72 +1,86 @@
 import React from 'react';
+
 class AboutUs extends React.Component {
   render(){
     return(
-    <div className="outer-about-container">
-      <div className="title-container">
+      <div className="about-us-background-container ">
+
+         {/* <div className="title-container">
         <h1>Meet the Team</h1> 
-      </div>
+      </div> */}
+      
+
+
+
+
       <div className= "about-us-container">
-        <div className="about-us-team-item colt grow">
+
+        <div className="about-us-polaroid colt grow">
           <div>
             <img src="../../../Colt_profile.JPG" alt="Colt Profile "/>
-              <h2>Colton Wilmouth</h2>
-              <h3>Team Lead</h3>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/colton-wilmouth-80b507132/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <h2>Team Lead</h2>
+              <h3>Colton Wilmouth</h3>
+            <ul className='polaroid-links-list'>
               <li><a href="https://coltw27.github.io/" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
               <li><a href="https://github.com/ColtW27" target="_blank" rel="noopener noreferrer">Github</a></li>
-              <li><a href="https://angel.co/u/colton-wilmouth" target="_blank" rel="noopener noreferrer">Angellist</a></li>
+              <li><a href="https://www.linkedin.com/in/colton-wilmouth-80b507132/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://angel.co/u/colton-wilmouth" target="_blank" rel="noopener noreferrer">AngelList</a></li>
             </ul>
           </div>
         </div>
 
-        <div className="about-us-team-item rahul grow">
+        <div className="about-us-polaroid rahul grow">
           <div>
             <img src="../../../rahul_jain.jpeg" alt="Rahul Profile"/>
-              <h2>Rahul Jain</h2>
-              <h3>Flex</h3>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/rahul-jain-ny/"target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <h2>Flex</h2>
+              <h3>Rahul Jain</h3>
+            <ul className='polaroid-links-list'>
               <li><a href="https://rahulj93.github.io/"target="_blank" rel="noopener noreferrer">Portfolio</a></li>
               <li><a href="https://github.com/rahulj93"target="_blank" rel="noopener noreferrer">Github</a></li>
-              <li><a href="https://angel.co/u/rahul-jain-ny"target="_blank" rel="noopener noreferrer">Angellist</a></li>
+              <li><a href="https://www.linkedin.com/in/rahul-jain-ny/"target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://angel.co/u/rahul-jain-ny"target="_blank" rel="noopener noreferrer">AngelList</a></li>
             </ul>
           </div>
         </div>
         
-        <div className="about-us-team-item carl grow">
+        <div className="about-us-polaroid carl grow">
           <div>
             <img src="../../../Carl_profile.JPG" alt="Carl Profile"/>
-              <h2>Carl Godlewski</h2>
-              <h3>Front End Lead</h3>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/carl-godlewski-b64b0512/"target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              <li><a href="https://angel.co/u/carl-godlewski" target="_blank" rel="noopener noreferrer">AngelList</a></li>
+              <h2>Front End Lead</h2>
+              <h3>Carl Godlewski</h3>
+            <ul className='polaroid-links-list'>
               <li><a href="https://cgodl.github.io/" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
               <li><a href="https://github.com/CGodl/" target="_blank" rel="noopener noreferrer">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/carl-godlewski-b64b0512/"target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://angel.co/u/carl-godlewski" target="_blank" rel="noopener noreferrer">AngelList</a></li>
             </ul>
           </div>
         </div>
 
-        <div className="about-us-team-item jon grow">
+        <div className="about-us-polaroid jon grow">
           <div>
             <img src="../../../Jon_profile.jpeg" alt="Jon Profile"/>
-              <h2>Jon Romero</h2>
-              <h3>Back End Lead</h3>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/jonathan-romero-ab325a165/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              <li><a href="https://angel.co/u/jonathan-romero-3" target="_blank" rel="noopener noreferrer">AngelList</a></li>
+              <h2>Back End Lead</h2>
+              <h3>Jon Romero</h3>
+            <ul className='polaroid-links-list'>
               <li><a href="https://Duskstone.github.io" target="_blank" rel="noopener noreferrer">Portfolio</a></li>
               <li><a href="https://github.com/duskstone" target="_blank" rel="noopener noreferrer">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/jonathan-romero-ab325a165/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://angel.co/u/jonathan-romero-3" target="_blank" rel="noopener noreferrer">AngelList</a></li>
             </ul>
           </div>
         </div>
+
+
       </div>
-    </div>
+
+      </div>
         )
     }
 
 }
 
 export default AboutUs;
+
+
+     
