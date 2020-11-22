ad2aceeec7454220fd0e453f65a2bfe82df0bc58
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-11-21 19:38:23 -0500

    Add logo to footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -6,6 +6,10 @@ class Footer extends React.Component{
   render(){
     return(
       <footer className="main-page-footer">
+        <div>
+          <img className="footer-logo"src={process.env.PUBLIC_URL + '/LessTravLogoTwoPinish.png'} />
+
+        </div>
         <div className="copyright">
           Copyright &copy; 2020 LessTravelled
         Made by: Carl Godlewski, Rahul Jain, Jonathan Romero, Colton Wilmouth
