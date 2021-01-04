f922d0eae68d9dacd915629fffff52cb43e2adcc
Carl Godlewski <cgodlew@gmail.com>
2021-01-04 10:37:52 -0500

    Added link to landing page to connect to main page

--- frontend/src/components/splash_page/landing_page.js
+++ frontend/src/components/splash_page/landing_page.js
@@ -1,19 +1,21 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-//Rename to Landing
 class LandingPage extends React.Component {
 
   render() {
     return (
      
       <div className='landing-page-container'>
+        <Link className='landing-page-link' to={'/main'}>
+          <div onClick={this.goToMain} className='landing-polaroid'>
+                <img  src='../../../images/splashPageImages/route-66.jpg' />
+                <h3>LessTravelled</h3>
+                <h4>See the world in all of its nitty-gritty detail</h4>
+          </div>
+        </Link>
 
-        <div className='landing-polaroid'>
-              <img  src='../../../images/splashPageImages/route-66.jpg' />
-              <h3>LessTravelled</h3>
-              <h4>See the world in all of its nitty-gritty detail</h4>
-        </div>
+
 
         <style type="text/css">
           {`.nav-container {display: none}`}
